198fa1cbc8b990af95e091b511db2a643308517f
priyapande961-glitch <priyapande961@gmail.com>
2026-06-26 19:33:13 +0530

    Add program to calculate volume of a sphere

---
+++ vol-of-sphere.cpp
@@ -0,0 +1,9 @@
+//calculating volume of a sphere
+#include <iostream>
+using namespace std;
+int main (){
+    float r;
+    cout<<"enter radius here;"<<endl;
+    cin>>r;
+    cout<<(4/3)*3.14*r*r*r<<endl;
+}
