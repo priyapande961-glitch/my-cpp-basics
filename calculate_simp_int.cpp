f893c6b80d4e6ce28830758115edd25938c38b0f
priyapande961-glitch <priyapande961@gmail.com>
2026-06-26 19:26:11 +0530

    Add simple interest calculation program
    Implement a program to calculate simple interest based on principal, rate, and time.

---
+++ calculate_simp_int.cpp
@@ -0,0 +1,15 @@
+//calculating simple interest
+#include <iostream>
+using namespace std;
+int main (){
+  float p;
+cout<<"enter p here;"<<endl;
+cin>>p;
+float r;
+cout<<"enter r here;"<<endl;
+cin>>r;
+float t;
+cout<<"enter t here;"<<endl;
+cin>>t;
+cout<<p*r*t/100<<endl;
+}
