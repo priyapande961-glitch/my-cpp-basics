a21f27c854f8a758f28225251ccad4fbf2d8cb02
priyapande961-glitch <priyapande961@gmail.com>
2026-06-26 19:08:58 +0530

    Implement circle area calculation program
    Added a program to calculate the area of a circle based on user input.

---
+++ circle_area.cpp
@@ -0,0 +1,11 @@
+//calculate area of circle using variables
+#include <iostream>
+using namespace std;
+int main (){
+ float r;
+  cout<<"enter radius here:"<<endl;
+  cin>>r;
+  float a = 3.14*r*r;
+  cout<<"area of the circle is: ";
+  cout<<a<<endl;
+}
