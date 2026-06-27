e038930abf76543c6fd89fe76b8df6c79ad19f51
priyapande961-glitch <priyapande961@gmail.com>
2026-06-27 13:48:30 +0530

    Add program to print half of a given number
    Implement a program to take an integer input and print half of it.

---
+++ half of the number.cpp
@@ -0,0 +1,9 @@
+//take x as an integer and print half of thr number
+#include <iostream>
+using namespace std;
+int main (){
+    float x;
+    cout<<"enter your nymber here:"<<endl;
+    cin>>x;
+    cout<<x/2<<endl;
+}
