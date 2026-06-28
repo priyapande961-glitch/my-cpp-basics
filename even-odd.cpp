0ce62a55c2dc6793b643b43a04d08c49ccfff5b3
priyapande961-glitch <priyapande961@gmail.com>
2026-06-28 21:04:25 +0530

    Add even-odd number checker in even-odd.cpp

---
+++ even-odd.cpp
@@ -0,0 +1,11 @@
+//even odd
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"enter number here: ";
+   cin>>n;
+   
+  if (n%2==0) cout<<"even number: "; 
+  if (n%2!=0) cout<<"odd number: ";
+}
