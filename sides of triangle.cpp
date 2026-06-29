a91fada74b86f884d76ef7fd427ee488c6f21af4
priyapande961-glitch <priyapande961@gmail.com>
2026-06-29 21:14:25 +0530

    Implement triangle sides validation program
    Added a program to check if three numbers can be the sides of a triangle.

---
+++ sides of triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n1;
+  cout<<"enter your numbers here: "<<endl;
+  cin>>n1;
+  int n2;
+  cout<<"enter your number here: "<<endl;
+  cin>>n2;
+  int n3;
+  cout<<"enter your number here: "<<endl;
+  cin>>n3;
+  if (n1+n2>n3) cout<<"it can be sides of triangle"<<endl;
+ else cout<<"it cant be sides of triangle";
+}
