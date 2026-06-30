044e9f290e195cedc59f1f9462e5d4d6450d4ed7
priyapande961-glitch <priyapande961@gmail.com>
2026-06-30 14:35:43 +0530

    Implement program to find the greatest of three numbers
    This program prompts the user to enter three numbers and determines the greatest among them.

---
+++ greatest of 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+  int a;
+ cout<<"enter 1st number: "<<endl;
+ cin>>a;
+ int b;
+ cout<<"enter 2nd number: "<<endl;
+ cin>>b;
+ int c;
+ cout<<"enter 3rd number: "<<endl;
+ cin>>c;
+ if (a>b and a>c) cout<<a;
+ if (c>b and a<c) cout<<c;
+ if (a<b and b>c) cout<<b;
+}
